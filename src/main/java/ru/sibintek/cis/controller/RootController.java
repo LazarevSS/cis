bbb37f633f1be1e79595d82a631bde5ec4a43233
LazarevSS <c.c.lazarev@gmail.com>
2018-06-28 20:19:30 +0300

    initial commit

--- src/main/java/ru/sibintek/cis/controller/RootController.java
+++ src/main/java/ru/sibintek/cis/controller/RootController.java
@@ -1,12 +1,13 @@
-package ru.sibintek.cis;
+package ru.sibintek.cis.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import ru.sibintek.cis.User;
 
 @Controller
-public class HelloController {
+public class RootController {
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public ModelAndView main() {
