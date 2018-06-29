5857dce7d74cea5d60007124aa6fc9d52e000104
LazarevSS <c.c.lazarev@gmail.com>
2018-06-29 11:52:32 +0300

    Добавлен hibernate

--- src/main/java/ru/sibintek/cis/controller/RootController.java
+++ src/main/java/ru/sibintek/cis/controller/RootController.java
@@ -1,10 +1,12 @@
 package ru.sibintek.cis.controller;
 
+import org.hibernate.Session;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 import ru.sibintek.cis.User;
+import ru.sibintek.cis.utils.HibernateSessionFactory;
 
 @Controller
 public class RootController {
@@ -15,6 +17,19 @@ public class RootController {
         modelAndView.addObject("userJSP", new User());
         modelAndView.setViewName("root");
         return modelAndView;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hibernate tutorial");
+
+        Session session = HibernateSessionFactory.getSessionFactory().openSession();
+
+        session.beginTransaction();
+
+
+        session.close();
+
+
     }
 
 }
